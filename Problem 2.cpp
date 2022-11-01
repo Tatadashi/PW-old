9e4046014b4fe5cc06d429a2cb1ad08481337f83
Tatadashi <kennyluu2742@gmail.com>
2022-10-31 21:52:12 -0700

    Add project files.

---
+++ Problem 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//program should display quarters, dimes, and nickels based on user input
+int main()
+{
+	int quarters, dimes, nickels;
+	cout << "Enter quarters, dimes, and nickels: ";
+	cin >> quarters >> dimes >> nickels;
+	cout << endl << "Numbers of quarters: " << endl;
+
+}
